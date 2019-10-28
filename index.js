5895184b41f0cae4c8d9eff7a051201d5e5103fb
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-28 12:23:34 -0700

    Push after lecture.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("\n**It's alive!**\n")
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Hello Node 23');
+})
+
+server.post('/echo', (req, res) => {
+  const data = req.body;
+  
+  res.json(data)
+})
+
+const port = 8000;
+server.listen(port, () => {
+  console.log("\n**API on port 8000**\n")
+})
